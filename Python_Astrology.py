3641528710956b2dbd4163c8f5f33dab75dfeec6
Drew Thomasson <superdrew100@Gmail.com>
2022-12-14 22:47:57 -0500

    created a random verb generating method

--- Python_Astrology.py
+++ Python_Astrology.py
@@ -1,13 +1,22 @@
-
+import random
 name = input("please enter your name so I may give you your REAL Astrology fortune: ").capitalize()
 nameList = []
 nameList.append(name)
 firstLetter = nameList[0][0]
 
+#this is a list of verbs to use
 verbs = ["criticize","tear","pin","cope","constitute","summon","calculate","tax","extend","change","situate","consult","arrest","laugh","enquire","reproduce","remark","lock","open","shrug","speed","ride","characterize","suspect","design","force","see","evaluate","precede","register","update","restrict","forget","land","head","shut","miss","weigh","swim","frown","breathe","arrive","wander","ease","protest","charge","crawl","emphasize","care","state"]
+
+#this will return a random verb from the list of verbs
+def randomVerb():
+    return verbs[random.randint(0,len(verbs))]
+print("RANDOM VERB TEST: "+ randomVerb())
+
+
+
 match name:
     case "Drew":
-        print("If you want to take your profession to the next level, you'll need to make some adjustments Possably by smiling. Identify the goals you wish to achieve such as assasinating JFK. Adjusting the pillars of your actual literal house that support your house may be necessary if you wish to pursue not having your house collaps(rlly tho get that shit checked out), achieve a new level of fire, or take a different direction in your career. Surround yourself with the right people.")
+        print("If you want to "+randomVerb()+" your profession to the next level, you'll need to "+randomVerb()+" some adjustments Possably by "+randomVerb()+" a deer. Identify the goals you wish to achieve such as assasinating JFK. "+randomVerb().capitalize()+" the pillars of your actual literal house that support your house may be necessary if you wish to "+randomVerb()+" not having your house collaps(rlly tho get that shit checked out), "+randomVerb()+" a new level of fire, or "+randomVerb()+" a different direction in your career. "+randomVerb().capitalize()+" yourself with the right people.")
 
     case "Andrew":
         print("Today is the day to show your worth, as you will undoubtedly face some difficult conditions in the office. As a result, maintaining your mental clarity will be crucial. Joining a team of passionate people is an opportunity to put your people skills and ability to persuade to good use. The vibrant environment will encourage you to work together to conquer any challenges.")
